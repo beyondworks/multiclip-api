83f23e3715a61946914d4da8300197e785ea306d
beyondworks <beyondworks.br@gmail.com>
2025-08-13 14:45:00 +0900

    Delete multiclip-api-starter directory

--- multiclip-api-starter/src/util.js
+++
@@ -1,2 +0,0 @@
-export const ok = (res, data) => res.json(data);
-export const err = (res, code, message) => res.status(code).json({ error: message });
--- multiclip-api-starter/src/worker.js
+++
@@ -1,1 +0,0 @@
-// TODO: 향후 BullMQ/Redis 도입 시 분리 예정
